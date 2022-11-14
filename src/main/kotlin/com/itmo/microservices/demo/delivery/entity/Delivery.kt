e2537665289745e0516f55a3515ea1e05552c20a
Kofeinich <agentkonch@mail.ru>
2022-11-14 14:48:16 +0300

    feat: Delivery.kt entity added

---
+++ src/main/kotlin/com/itmo/microservices/demo/delivery/entity/Delivery.kt
@@ -0,0 +1,35 @@
+package com.itmo.microservices.demo.delivery.entity
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+class Delivery {
+
+
+    @Id
+    @GeneratedValue
+    var from: String? = null
+    var to: String? = null
+    var orderStatus: String? = null
+    var orderName: String? = null
+    var orderId: String? = null
+    var userName: String? = null
+    var userId: String? = null
+    var courierName: String? = null
+    var comment: String? = null
+
+    constructor()
+
+    constructor(from: String, to: String, orderStatus: String, orderName: String, orderId: String, userName: String, userId: String, courierName: String, comment: String) {
+        this.from = from
+        this.to = to
+        this.orderId = orderId
+        this.orderName = orderName
+        this.orderStatus = orderStatus
+        this.userName = userName
+        this.userId = userId
+        this.courierName = courierName
+        this.comment = comment
+    }
+}
